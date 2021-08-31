49b0932b76941834c925d00717f3e6d166301407
eilerglen <83466883+eilerglen@users.noreply.github.com>
2021-08-31 18:07:22 +0300

    Пишем валидность форм

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,16 @@
-/*инициация всех попапов на странице*/ 
+/*инициация всех попапов на странице*/
 let popupEdit = document.querySelector(".popup_edit");
 let popupNewCard = document.querySelector('.popup_new-card');
 let popupImage = document.querySelector('.popup_image');
 let placeImageScale = popupImage.querySelector('.popup__image');
 let placeImageScaleCaption = popupImage.querySelector('.popup__caption');
 
-/*Кнопки открытия попапов */ 
+/*Кнопки открытия попапов */
 let editButton = document.querySelector('.profile__edit-button');
 let addButton = document.querySelector('.profile__add-button');
 /*let imageButton = document.querySelector('.profile__add-button');*/
 
-/*Кнопки закрытия попапов */ 
+/*Кнопки закрытия попапов */
 let popupCloseEdit = popupEdit.querySelector('.popup__close');
 let popupCloseNewCard = popupNewCard.querySelector('.popup__close');
 let popupCloseImage = popupImage.querySelector('.popup__close');
@@ -33,7 +33,7 @@ function closePopup(popup) {
 
 function handlerClosePopupEsc (evt) {
   if(evt.key = 'Escape') {
-    
+
   }
 }
 
@@ -42,7 +42,7 @@ editButton.addEventListener('click', () => {
   inputEditProfileName.value = profileName.textContent;
   inputEditProfileProf.value = profileProfession.textContent;
   openPopup(popupEdit);
-  
+
 });
 
 //Открытие popup добавления формы
@@ -84,11 +84,11 @@ function editFormSubmit (evt) {
 /* Вызываем функцию редактирования профиля как колбэке на обработчике формы*/
 formEditProfileInfo.addEventListener('submit', editFormSubmit);
 
-/*Обозначаем контейнер, куда карточки могут добавляться*/ 
+/*Обозначаем контейнер, куда карточки могут добавляться*/
 const placesList = document.querySelector('.places__list');
 let templatePlace = document.querySelector('#template-place').content;
 
-/*Функция создания карточки*/ 
+/*Функция создания карточки*/
 function createCard(data) {
   let placeItem = templatePlace.querySelector('.place').cloneNode(true);
   /*берем данные из словаря или формы для рендеринга контента карточки*/
@@ -104,7 +104,7 @@ function createCard(data) {
     const eventTarget = evt.target;
     eventTarget.classList.toggle('place__icon_active');
   });
- 
+
  /*Вешаем обработчик на удаление карточки. Здесь можно простой колбэк или запилить отдельную функцию*/
   let placeDeleteButton = placeItem.querySelector('.place__delete-button');
 
@@ -117,8 +117,8 @@ function createCard(data) {
     placeImageScale.src = data.link;
     placeImageScale.alt = data.name;
     placeImageScaleCaption.textContent = data.name;
-    openPopup(popupImage); 
-    
+    openPopup(popupImage);
+
 
 
   });
@@ -130,19 +130,18 @@ function createCard(data) {
   return placeItem;
 }
 
-/*Функция добавления карточки на страницу*/ 
+/*Функция добавления карточки на страницу*/
 function addCard (data, container) {
   let place = createCard(data);
   container.prepend(place);
 }
 
 
-/*Вызов функции добавления карточек "из коробки на страницу" на страницу в цикле по массиву*/ 
+/*Вызов функции добавления карточек "из коробки на страницу" на страницу в цикле по массиву*/
 
 initialCards.forEach((item) => {
   addCard(item, placesList)
 });
-
 
 
 /*Вызов функции создания новой карточки*/
@@ -155,30 +154,33 @@ function addFormSubmit (evt) {
 
 formAddNewPlace.addEventListener('submit', addFormSubmit);
 
-
 const obj = {
-  formSelector: 'popup__form',
-  inputSelector: 'popup__input',
-  submitButtonSelector: '.popup__button-submit',
-  errorClass: 'popup__error'
+  formSelector:'.popup__form',
+  inputSelector:'.popup__input',
+  submitButtonSelector:'.popup__button-submit',
+  errorClass: '.popup__error_active'
 }
 
 enableValidation(obj)
 
 function enableValidation({formSelector, inputSelector, submitButtonSelector, errorClass}) {
-  const forms = Array.from(document.querySelectorAll(formSelector))
+  const forms = Array.from(document.querySelectorAll(formSelector));
   forms.forEach(form => {
     form.addEventListener('submit', e => e.preventDefault())
+
     const inputs = form.querySelectorAll(inputSelector)
-    inputs.addEventListener('input', e => {
-      console.log(input.validity)
-      if(input.validity.valid) {
-         
-      }
-      else {
-        
-      }
+    inputs.forEach(input => {
+      input.addEventListener('input', e=> {
+       if(input.validity.valid) {
+        //** скрыть ошибку под полем*/
+       } else {
+         const errorPlace = document.querySelector(`.${input.id}-error`);
+         console.log(errorPlace);
+         /*errorPlace.textContent = input.validationMessage;
+         errorPlace.classListAdd(errorClass);*/
+           //** показать ошибку под полем*/
+         }
+      })
     })
   })
-
 }
